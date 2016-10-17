e1430c94dfd303c820d32926c7feb29605a4f5ef
王丽云 <wangliyun@siwei.tech>
2016-10-17 13:58:54 +0800

    给编辑页面增加了轮播图

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require jssor.slider-21.1.6.mini
+//= require jquery-1.11.3.min
+
